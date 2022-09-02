509b51aa89a4d3f76d605f519ef594fa6ff38ceb
rahbar11 <rahbarkhan97@gmail.com>
2022-09-02 17:34:26 +0530

    add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 require("./db/connection")
@@ -7,6 +8,9 @@ require("./db/connection")
 const port = process.env.PORT
 
 const app = express()
+
+app.use(cors())
+
 const server = http.createServer(app);
 const io = new Server(server, { 
     cors: {
