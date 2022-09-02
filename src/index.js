b7c4c392b5ef6029c7a99ffca946375b5fd5a28d
Rahbar Khan <rahbarkhan97@gmail.com>
2022-09-02 09:22:26 +0530

    changes for heroku

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const http = require("http");
 const { Server } = require("socket.io");
 require("./db/connection")
 
+
+const port = process.env.PORT
+
 const app = express()
 const server = http.createServer(app);
 const io = new Server(server, { 
@@ -14,6 +17,6 @@ const io = new Server(server, {
 require('./sockets')(io);
 
 
-server.listen(3001, () => {
-    console.log('listening on *:3001');
+server.listen(port, () => {
+    console.log(`listening: ${port}`);
   });
